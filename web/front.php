087b59bcbfb9e73cce76809c68249e475862673a
programmingArrow <nishant.kashyap2104@gmail.com>
2020-03-03 23:52:06 +0530

    adjusted map array

--- web/front.php
+++ web/front.php
@@ -10,7 +10,7 @@ $response = new Response;
 
 $map = [
     '/hello' => __DIR__ . '../src/pages/hello.php',
-    '/bye' => __DIR__ . '..src/pages /bye.php',
+    '/bye' => __DIR__ . '../src/pages/bye.php',
 ];
 
 $path = $request->getPathInfo();
